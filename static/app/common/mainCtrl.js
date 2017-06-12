227889790073a38ae781846a6f6bb84b99639fd9
Mykhailo Zheliaskov <mykhailo.zheliaskov@decerto.pl>
2017-06-13 01:51:54 +0200

    fix redirection for empty url

--- static/app/common/mainCtrl.js
+++ static/app/common/mainCtrl.js
@@ -6,14 +6,16 @@ wsRoomApp.controller('mainCtrl', function ($scope, $location, $rootScope, AuthSe
     $rootScope.$on('$locationChangeStart', function (event, next, current) {
         UserService.getLoggedIn(function (loggedIn) {
             $rootScope.globals.user = loggedIn;
-            var restrictedPage = $.inArray($location.path(), ['/login', '/']) === -1;
+            var restrictedPage = $.inArray($location.path(), ['/login']) === -1;
             if (restrictedPage && !loggedIn) {
                 $location.path('/login');
                 return;
             }
             if (!restrictedPage && loggedIn) {
                 $location.path('/games');
+                return;
             }
+            $location.path(loggedIn ? '/games' : '/login');
         });
     });
 
